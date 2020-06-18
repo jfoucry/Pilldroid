08beb8323aa91e79c0ba490a882584481b37f792
jacques <jacques+commit@foucry.net>
2020-06-18 20:53:11 +0200

    optimize import

--- app/src/main/java/net/foucry/pilldroid/MedicamentDetailActivity.java
+++ app/src/main/java/net/foucry/pilldroid/MedicamentDetailActivity.java
@@ -2,7 +2,6 @@ package net.foucry.pilldroid;
 
 import android.content.Intent;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
