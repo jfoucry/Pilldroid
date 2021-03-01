1ae2994045ab6bf3f24026643bf1e459b6751dcc
jacques <jacques+commit@foucry.net>
2021-03-01 18:52:37 +0100

    Small changes. Not really important this code will be removed

--- app/src/main/java/net/foucry/pilldroid/PillDroidJobService.java
+++ app/src/main/java/net/foucry/pilldroid/PillDroidJobService.java
@@ -6,7 +6,6 @@ import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
@@ -86,7 +85,7 @@ public class PillDroidJobService extends JobService {
     /**
      * Schedule Notification for the delay
      */
-    private void scheduleNotification() {
+    void scheduleNotification() {
         Log.d(TAG, "schedule notification");
         createNotificationChannel();
         Intent intent = new Intent(this, MedicamentListActivity.class);
