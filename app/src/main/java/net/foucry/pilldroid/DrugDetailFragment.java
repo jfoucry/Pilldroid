fd25a41a113295844779812524dead100f603fbd
jacques <jacques+commit@foucry.net>
2021-07-09 11:18:13 +0200

    Optimize import

--- app/src/main/java/net/foucry/pilldroid/DrugDetailFragment.java
+++ app/src/main/java/net/foucry/pilldroid/DrugDetailFragment.java
@@ -1,15 +1,15 @@
 package net.foucry.pilldroid;
 
 import android.app.Activity;
-
-import com.google.android.material.appbar.CollapsingToolbarLayout;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 /**
  * A fragment representing a single Drug detail screen.
