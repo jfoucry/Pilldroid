88c534e71968258aa0bcf54dfed46529456eef80
jacques <jacques+commit@foucry.net>
2022-03-07 22:09:23 +0100

    use correct import add getters and setters

--- app/src/main/java/net/foucry/pilldroid/models/Medic.java
+++ app/src/main/java/net/foucry/pilldroid/models/Medic.java
@@ -1,13 +1,13 @@
 package net.foucry.pilldroid.models;
 
-import android.arch.persitence.room.Entity;
-import android.arch.persitence.room.PrimaryKey;
-import android.support.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.annotation.NonNull;
 
 @Entity(tableName = "medics")
-public class medic {
+public class Medic {
   @PrimaryKey
-  @NonNull private Long id;
+  @NonNull private final Integer id;
   private String  cis;
   private String  cip13;
   private String  name;
@@ -15,8 +15,102 @@ public class medic {
   private String  presentation;
   private Long    stock;
   private Long    take;
-  private Int     warning;
-  private Int     alert;
+  private Integer warning;
+  private Integer alert;
   private Long    last_update;
+
+  public Medic(@NonNull Integer id) {
+    this.id = id;
+  }
+
+  public void setCis(String cis)
+{
+  this.cis = cis;
 }
+
+
+  public void setCip13(String cip13) {
+    this.cip13 = cip13;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+public void setAdministration_mode(String administration_mode) {
+
+  this.administration_mode = administration_mode;
+}
+
+public void setPresentation(String presentation) {
+  this.presentation = presentation;
+}
+public void setStock(Long stock) {
+  this.stock = stock;
+}
+
+public void setTake(Long take) {
+  this.take = take;
+}
+
+public void setWarning(Integer warning) {
+  this.warning = warning;
+}
+
+public void setAlert(Integer alert) {
+  this.alert = alert;
+}
+
+public void setLast_update(Long last_update) {
+  this.last_update = last_update;
+}
+
+@NonNull
+Integer getId() {
+  return  this.id;
+}
+
+String getCis() {
+  return this.cis;
+}
+
+  public Integer getWarning() {
+    return warning;
+  }
+
+  public String getCip13() {
+    return cip13;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAdministration_mode() {
+    return administration_mode;
+  }
+
+  public String getPresentation() {
+    return presentation;
+  }
+
+  public Long getStock() {
+    return stock;
+  }
+
+  public Long getTake() {
+    return take;
+  }
+
+  public Integer getAlert() {
+    return alert;
+  }
+
+  public Long getLast_update() {
+    return last_update;
+  }
+}
+
+
+
 
