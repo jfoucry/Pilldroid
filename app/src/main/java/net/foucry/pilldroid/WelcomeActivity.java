6390b7977c861829764d9c444880bb80e71e76d7
jacques <jacques+commit@foucry.net>
2022-03-13 12:50:40 +0100

    change file names

--- app/src/main/java/net/foucry/pilldroid/WelcomeActivity.java
+++ app/src/main/java/net/foucry/pilldroid/WelcomeActivity.java
@@ -45,7 +45,7 @@ public class WelcomeActivity extends AppCompatActivity {
         }
 
         prefManager.setFirstTimeLaunch(false);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.WelcomeActivity);
 
         setFullScreen();
 
