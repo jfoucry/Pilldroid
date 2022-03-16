f7bdb7d6fb0d2f22b2c7bb26f123296b6d047b8c
jacques <jacques+commit@foucry.net>
2022-03-16 05:44:19 +0100

    rename Medic in Prescription and refactor code

--- app/src/main/java/net/foucry/pilldroid/PrescriptionDatabase.java
+++ app/src/main/java/net/foucry/pilldroid/PrescriptionDatabase.java
@@ -4,18 +4,18 @@ import androidx.room.AutoMigration;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-import net.foucry.pilldroid.dao.MedicDAO;
-import net.foucry.pilldroid.models.Medic;
+import net.foucry.pilldroid.dao.PrescriptionsDAO;
+import net.foucry.pilldroid.models.Prescription;
 
 @Database(
         version = 3,
-        entities = {Medic.class},
+        entities = {Prescription.class},
         autoMigrations = {
                 @AutoMigration(from = 1, to = 2),
                 @AutoMigration(from = 2, to = 3),
         }
 )
 
-public abstract class PilldroidDatabase extends RoomDatabase {
-    public abstract MedicDAO getMedicDAO();
+public abstract class PrescriptionDatabase extends RoomDatabase {
+    public abstract PrescriptionsDAO getPrescriptionsDAO();
 }
