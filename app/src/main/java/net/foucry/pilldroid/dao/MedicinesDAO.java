b2f54a81c495d2554e6a63379afbb298d1baf273
jacques <jacques+commit@foucry.net>
2022-03-17 18:27:01 +0100

    Use drugs tablename

--- app/src/main/java/net/foucry/pilldroid/dao/MedicinesDAO.java
+++ app/src/main/java/net/foucry/pilldroid/dao/MedicinesDAO.java
@@ -9,12 +9,12 @@ import java.util.List;
 
 @Dao
 public interface MedicinesDAO {
-    @Query("SELECT * FROM medicines")
+    @Query("SELECT * FROM drugs")
     List<Medicine> getAllMedicines();
 
-    @Query("SELECT * FROM medicines WHERE cip13 = :cip13")
+    @Query("SELECT * FROM drugs WHERE cip13 = :cip13")
     Medicine getMedicineByCIP13(String cip13);
 
-    @Query("SELECT count(*) FROM medicines")
+    @Query("SELECT count(*) FROM drugs")
     int getMedicineCount();
 }
