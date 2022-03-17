df68ee96fe8b30100e88c314496e758e28281e0e
jacques <jacques+commit@foucry.net>
2022-03-17 18:27:17 +0100

    optimize import

--- app/src/main/java/net/foucry/pilldroid/databases/MedicineDatabase.java
+++ app/src/main/java/net/foucry/pilldroid/databases/MedicineDatabase.java
@@ -1,13 +1,10 @@
 package net.foucry.pilldroid.databases;
 
-import androidx.room.AutoMigration;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 import net.foucry.pilldroid.dao.MedicinesDAO;
-import net.foucry.pilldroid.dao.PrescriptionsDAO;
 import net.foucry.pilldroid.models.Medicine;
-import net.foucry.pilldroid.models.Prescription;
 
 @Database(
         version = 1,
